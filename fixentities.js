d5f1809bd8938b565d06d721e73e71771f78e90b
ccato <cato.christopher@gmail.com>
2015-10-01 15:10:09 +0200

    added check for CKEDITOR and updated info file

--- fixentities.js
+++ fixentities.js
@@ -1,13 +1,14 @@
 /*
-    Removes pasted non-breaking spaces in CKEditor 4.x
-    Does not disable basicEntitites which would be opening a XSS hole
+ Removes pasted non-breaking spaces in CKEditor 4.x
+ Does not disable basicEntitites which would be opening a XSS hole
  */
 
-
-CKEDITOR.on('instanceReady', function (ev) {
-    ev.editor.on('paste', function (evt) {
-        if (evt.data["html"]) {
-            evt.data["html"].replace(/&nbsp;/g, '');
-        }
-    }, null, null, 9);
-});
+if (CKEDITOR) {
+    CKEDITOR.on('instanceReady', function (ev) {
+        ev.editor.on('paste', function (evt) {
+            if (evt.data["html"]) {
+                evt.data["html"].replace(/&nbsp;/g, '');
+            }
+        }, null, null, 9);
+    });
+}
